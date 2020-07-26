61110e141be39d95830ea3a671eee61b33328d60
AppleJacksApple <applejacksapple@gmail.com>
2020-07-25 17:55:10 -0700

    Set up create habit flow. Added view controller swift files

---
+++ Habitude/View Controllers/Create1ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Create1ViewController.swift
+//  Habitude
+//
+//  Created by Jack Le on 7/25/20.
+//
+
+import UIKit
+
+class Create1ViewController: UIViewController {
+    
+    @IBOutlet weak var habitTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    
+}
